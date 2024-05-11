0b567e2d7cdabe03281db316b89060a5021b7653
shaikriyan817 <shaikriyan817@gmail.com>
2024-05-11 16:43:19 +0530

    testing

---
+++ demo/test1.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define deb(x) cout<<#x<<" = "; print(x); cout<<endl;
+
+template<class T> void print(T a){ cout<<a;}
+
+template<typename T>
+void print(vector<T> v){
+	cout<<"[ ";
+	for(int i=0;i<v.size();i++){
+		print(v[i]);
+		cout<<" ";
+	}
+	cout<<"]";
+}
+
+
+
+int main()
+{
+
+
+	return 0;
+}
+
+
+
+
+/* D:\LazyPractice\first.cpp */
+/**/
+/**/
+/**/
+/* D:\LazyPractice\first.cpp */
+/* D:\LazyPractice\first.cpp */
+/* D:\ */
+/* D:\LazyPractice\first.cpp */
+/* D:\LazyPractice\first.cpp */
+/* D:\LazyPractice\first.cpp */
+/* D:\LazyPractice\first.cpp */
+/* D:\Hellothere\first.cpp */
+/**/
+/**/
+/* LazyPractice\first.cpp */
+/**/
+/**/
